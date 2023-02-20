41fc8439d1a081948ca78adf845c7947a35b5656
Adrienne Slaughter <ahslaughter@northeastern.edu>
2023-02-20 09:36:10 -0800

    Cleaning up before turning into a class

--- monday/robot_control_panel.py
+++ monday/robot_control_panel.py
@@ -14,70 +14,77 @@ import time
 
 from hid_gamepad import Gamepad
 
+## Turn the indicator light on
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 led.value = True
 
-print("starting, yo")
-print(usb_hid.devices)
 gp = Gamepad(usb_hid.devices)
-
-print("gamepad created")
 
 # Create some buttons. The physical buttons are connected
 # to ground on one side and these and these pins on the other.
-## button_pins = (board.D2, board.D3, board.D4, board.D5)
-## button_pins = (board.D2, board.D3, board.D4, board.D5)
+button_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5, board.GP6, board.GP7 )
 
 # Map the buttons to button numbers on the Gamepad.
 # gamepad_buttons[i] will send that button number when buttons[i]
 # is pushed.
-gamepad_buttons = (1, 2, 8, 15)
+gamepad_buttons = (1, 2, 3, 4, 5, 6, 7, 8)
 
-#buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
-#for button in buttons:
-#    button.direction = digitalio.Direction.INPUT
-#    button.pull = digitalio.Pull.UP
+buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
+for button in buttons:
+   button.direction = digitalio.Direction.INPUT
+   button.pull = digitalio.Pull.UP
 
-button_pin = board.GP15
-button1 = digitalio.DigitalInOut(button_pin)
-button1.direction = digitalio.Direction.INPUT
-button1.pull = digitalio.Pull.UP
-button2_pin = board.GP14
-button2 = digitalio.DigitalInOut(button2_pin)
-button2.direction = digitalio.Direction.INPUT
-button2.pull = digitalio.Pull.UP
+# Create some toggles. The physical toggles are connected
+# to ground on one side and these and these pins on the other.
+toggle_pins = (board.GP10, board.GP11, board.GP12, board.GP13)
 
-joystick_up_pin = board.GP10
-joystick_down_pin = board.GP11
-joystick_right_pin = board.GP12
-joystick_left_pin = board.GP13
+# Map the toggles to toggle numbers on the Gamepad.
+# gamepad_toggles[i] will send that toggle number when toggles[i]
+# is pushed.
+toggles_switches = (1, 2, 3, 4)
+
+toggles = [digitalio.DigitalInOut(pin) for pin in toggle_pins]
+for toggle in toggles:
+   toggle.direction = digitalio.Direction.INPUT
+   toggle.pull = digitalio.Pull.UP
+
+# Create the Joystick hat switches 
+joystick_up_pin = board.GP18
+joystick_down_pin = board.GP19
+joystick_right_pin = board.GP20
+joystick_left_pin = board.GP21
 
 joystick_up = digitalio.DigitalInOut(joystick_up_pin)
 joystick_up.direction = digitalio.Direction.INPUT
-joystick_up.pull = digitalio.Pull.DOWN
+joystick_up.pull = digitalio.Pull.UP
 
 joystick_down = digitalio.DigitalInOut(joystick_down_pin)
 joystick_down.direction = digitalio.Direction.INPUT
 joystick_down.pull = digitalio.Pull.UP
 
+joystick_left = digitalio.DigitalInOut(joystick_left_pin)
+joystick_left.direction = digitalio.Direction.INPUT
+joystick_left.pull = digitalio.Pull.UP
 
-# Connect an analog two-axis joystick to A4 and A5.
-ax = analogio.AnalogIn(board.A0)
+joystick_right = digitalio.DigitalInOut(joystick_right_pin)
+joystick_right.direction = digitalio.Direction.INPUT
+joystick_right.pull = digitalio.Pull.UP
+
+# Set up the analog sliders 
+sliderX = analogio.AnalogIn(board.A0)
 slider = analogio.AnalogIn(board.A2)
-ay = analogio.AnalogIn(board.A1)
+sliderY = analogio.AnalogIn(board.A1)
 
-
-# ax = digitalio.DigitalInOut(board.GP10)
-# ax.direction = digitalio.Direction.INPUT
-# ay = digitalio.DigitalInOut(board.GP11)
-# ay.direction = digitalio.Direction.INPUT
 
 # Equivalent of Arduino's map() function.
 def range_map(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
 
+## Takes in the value of the hat switches (which ones are on/off)
+## and returns a Gamepad JOYSTICK direction
+## TODO: we can totally do this more efficiently, but eh
 def direction_map(up, down, left, right):
     if up:
         if left:
@@ -98,46 +105,49 @@ def direction_map(up, down, left, right):
     return Gamepad.JOYSTICK_HOME
 
 
+def send_buttons: 
+    # Buttons are grounded when pressed (.value = False).
+    for i, button in enumerate(buttons):
+        gamepad_button_num = gamepad_buttons[i]
+        if button.value:
+            gp.release_buttons(gamepad_button_num)
+            print(" release", gamepad_button_num, end="")
+        else:
+            gp.press_buttons(gamepad_button_num)
+            print(" press", gamepad_button_num, end="")   
+
+def send_toggles: 
+    # Toggles are grounded when pressed (.value = False).
+    for i, toggle in enumerate(toggles):
+        gamepad_toggle_num = gamepad_toggles[i]
+        if toggle.value:
+            gp.toggle_off(gamepad_toggle_num) ## TODO: Make sure this is the right direction
+            print(" toggleOff: ", gamepad_toggle_num, end="")
+        else:
+            gp.toggle_on(gamepad_toggle_num)
+            print(" toggleOn: ", gamepad_toggle_num, end="")   
+
+def send_sliders:
+    # Convert range[0, 65535] to 0 to 255
+    # gp.move_joysticks(
+    #     x=range_map(ax.value, 0, 65535, -127, 127),
+    #     y=range_map(ay.value, 0, 65535, -127, 127),
+    # )
+    gp.set_sliderX(sliderX.value, 0, 65535, 0, 255)
+    gp.set_sliderY(sliderY.value, 0, 65535, 0, 255)
+
+def send_joystick: 
+    gp.set_digital_joystick(direction_map(~joystick_up.value, 
+                                          joystick_down.value, 
+                                          joystick_left.value, 
+                                          joystick_right.value))
+
 while True:
     # print("loop")
-
-    gp.press_buttons(2)
-    print("Turn toggle1 on")
-    gp.toggle_on(1)
-    time.sleep(1)
-    print("Turn toggle2 on")
-    gp.toggle_on(2)
-    time.sleep(1)
-    print("Turn toggle3 on")
-    gp.toggle_on(3)
-    time.sleep(1)
-    print("Turn toggle4 on")
-    gp.toggle_on(4)
-    time.sleep(1)
-    gp.toggle_off(1,2,3,4)
-
-    print("Set sliderX")
-    gp.set_sliderX(85)
-    time.sleep(1)
-    print("Set sliderY")
-    gp.set_sliderY(127) ## TODO: Getting bigger than 127
-    time.sleep(1)
-
-    print("Release button2")
-    gp.release_buttons(2)
-    time.sleep(1)
-    print("Set joystick North")
-    gp.set_analog_joystick(Gamepad.JOYSTICK_NORTH)
-    time.sleep(1)
-    print("Set joystick NW")
-    gp.set_analog_joystick(Gamepad.JOYSTICK_NW) ## Setting this causes overflow
-    time.sleep(1)
-    print("Set joystick South")
-    gp.set_analog_joystick(Gamepad.JOYSTICK_SOUTH)
-    time.sleep(1)
-
-    print("Set joystick Home")
-    gp.set_analog_joystick(Gamepad.JOYSTICK_HOME)
+    send_buttons()
+    send_toggles()
+    send_sliders()
+    send_joystick()
 
     time.sleep(1)
 
